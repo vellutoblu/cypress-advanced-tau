135709c4e01b1f87f7aaab40f2174c6ff0f60e23
Matias <matiasvaca993@gmail.com>
2024-04-24 23:15:58 -0300

    import list json and add in loop

--- cypress/e2e/specs.cy.ts
+++ cypress/e2e/specs.cy.ts
@@ -1,5 +1,21 @@
+import * as lists from '../fixtures/lists.json'
+
 it('playing with typescript', () =>{
+
+    cy.intercept('POST', '/api/lists')
+        .as('listCreate')
+
     cy.visit('/board/1')
 
+    lists.forEach(list => {
+        cy.get('[data-cy="add-list-input"]')
+        .type(`${list.name}{enter}`)
 
+    cy.wait('@listCreate')
+
+    })
+
+   
+
+        
 })
