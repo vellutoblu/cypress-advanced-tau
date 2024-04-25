87f40938c6d5a914e99ba4264e13f47e7a809942
Matias <matiasvaca993@gmail.com>
2024-04-25 20:43:39 -0300

    cypress.log information added

--- cypress/support/commands/getByPlaceholder.ts
+++ cypress/support/commands/getByPlaceholder.ts
@@ -1,3 +1,13 @@
 Cypress.Commands.add('getByPlaceholder', (input: string)=>{
+
+    Cypress.log({
+        displayName: 'getByPlaceholder',
+        message: input,
+        consoleProps(){
+            return {
+                selector: input
+            }
+        }
+        })
     return cy.get(`[placeholder="${input}"]`)
 } )
